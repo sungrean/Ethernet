73f75ad14f680f7dd0d9b45c2aaa9e540b85882a
sun_grean@foxmail.com <sun_grean@foxmail.com>
2019-11-28 09:09:22 +0800

    打开混杂模式

--- Ethernet/Form1.Designer.cs
+++ Ethernet/Form1.Designer.cs
@@ -28,13 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtRecv = new System.Windows.Forms.TextBox();
+            this.txtTips = new System.Windows.Forms.TextBox();
+            this.EthernetPort = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OpenNet = new System.Windows.Forms.Button();
+            this.CloseNet = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 69.97423F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.02577F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.CloseNet);
+            this.groupBox1.Controls.Add(this.OpenNet);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(562, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(235, 444);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "操作";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.EthernetPort);
+            this.groupBox2.Controls.Add(this.txtTips);
+            this.groupBox2.Controls.Add(this.txtRecv);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(3, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(553, 444);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "接收数据";
+            // 
+            // txtRecv
+            // 
+            this.txtRecv.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRecv.Location = new System.Drawing.Point(6, 167);
+            this.txtRecv.Multiline = true;
+            this.txtRecv.Name = "txtRecv";
+            this.txtRecv.Size = new System.Drawing.Size(541, 268);
+            this.txtRecv.TabIndex = 0;
+            // 
+            // txtTips
+            // 
+            this.txtTips.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTips.Location = new System.Drawing.Point(6, 55);
+            this.txtTips.Multiline = true;
+            this.txtTips.Name = "txtTips";
+            this.txtTips.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtTips.Size = new System.Drawing.Size(541, 106);
+            this.txtTips.TabIndex = 1;
+            // 
+            // EthernetPort
+            // 
+            this.EthernetPort.FormattingEnabled = true;
+            this.EthernetPort.Location = new System.Drawing.Point(367, 20);
+            this.EthernetPort.Name = "EthernetPort";
+            this.EthernetPort.Size = new System.Drawing.Size(180, 20);
+            this.EthernetPort.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(296, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "选择网口：";
+            // 
+            // OpenNet
+            // 
+            this.OpenNet.Location = new System.Drawing.Point(95, 23);
+            this.OpenNet.Name = "OpenNet";
+            this.OpenNet.Size = new System.Drawing.Size(75, 23);
+            this.OpenNet.TabIndex = 0;
+            this.OpenNet.Text = "打开网口";
+            this.OpenNet.UseVisualStyleBackColor = true;
+            this.OpenNet.Click += new System.EventHandler(this.OpenNet_Click);
+            // 
+            // CloseNet
+            // 
+            this.CloseNet.Enabled = false;
+            this.CloseNet.Location = new System.Drawing.Point(95, 66);
+            this.CloseNet.Name = "CloseNet";
+            this.CloseNet.Size = new System.Drawing.Size(75, 23);
+            this.CloseNet.TabIndex = 1;
+            this.CloseNet.Text = "关闭网口";
+            this.CloseNet.UseVisualStyleBackColor = true;
+            this.CloseNet.Click += new System.EventHandler(this.CloseNet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtTips;
+        private System.Windows.Forms.TextBox txtRecv;
+        private System.Windows.Forms.Button CloseNet;
+        private System.Windows.Forms.Button OpenNet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox EthernetPort;
     }
 }
 
--- Ethernet/Form1.cs
+++ Ethernet/Form1.cs
@@ -1,19 +1,150 @@
-﻿using System;
+﻿using SharpPcap;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Ethernet
 {
     public partial class Form1 : Form
     {
+        #region 属性和字段
+
+        private ICaptureDevice device;//当前设备
+
+        private Thread backgroundThread;//封装原始包的进程
+
+        private bool backgroundThreadStop;//是否手动停止后台线程
+
+        private object QueueLock = new object();
+
+        private List<RawCapture> PacketQueue = new List<RawCapture>();//接收的原始包数据 
+
+        private int packetCount;//接收包数量和序号
+
+        private ICaptureStatistics captureStatistics;//统计信息 
+
+        private bool statisticsUiNeedsUpdate = false;
+        #endregion
         public Form1()
         {
+            CheckForIllegalCrossThreadCalls = false;
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string ver = SharpPcap.Version.VersionString;
+            //Console.WriteLine("SharpPcap {0}, Example1.IfList.cs", ver);
+            txtTips.AppendText(string.Format("SharpPcap {0}, Example1.IfList.cs\r\n", ver));
+            // Retrieve the device list
+            var devices = CaptureDeviceList.Instance;
+            // If no devices were found print an error
+            if (devices.Count < 1)
+            {
+                txtTips.AppendText(string.Format("No devices were found on this machine\r\n"));
+                return;
+            }
+            txtTips.AppendText(string.Format("\r\nThe following devices are available on this machine:"));
+            txtTips.AppendText(string.Format("----------------------------------------------------\r\n"));
+            /* Scan the list printing every entry */
+            foreach (var dev in devices)
+            {
+                txtTips.AppendText(string.Format("{0}\r\n\r\n\r\n", dev.ToString()));
+                EthernetPort.Items.Add("" + dev.Description.Substring(16));
+            }
+             
+            //Console.ReadLine();
+            //————————————————
+            //版权声明：本文为CSDN博主「gaohuaid」的原创文章，遵循 CC 4.0 BY - SA 版权协议，转载请附上原文出处链接及本声明。
+            //原文链接：https://blog.csdn.net/gaohuaid/article/details/8853587
+        }
+
+        private void OpenNet_Click(object sender, EventArgs e)
+        {
+            if(EthernetPort.SelectedIndex>-1)
+            {
+                StartCapture(EthernetPort.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("请选择网口");
+            }
+        }
+
+        private void StartCapture(int itemIndex)
+        {
+            device = CaptureDeviceList.Instance[itemIndex];
+            OpenNet.Enabled = false;
+            CloseNet.Enabled = true;
+            device.OnPacketArrival += new PacketArrivalEventHandler(device_OnPacketArrival);
+             
+
+            device.Open(DeviceMode.Promiscuous);        //打开混杂模式
+            device.StartCapture();
+            if (device.Started)
+            { 
+                backgroundThread = new Thread(BackgroundThread); 
+            }
+
+        }
+        //后台线程进行贞处理
+        private void BackgroundThread()
+        {
+            while (!backgroundThreadStop)
+            {
+                bool shouldSleep = true;
+
+                lock (QueueLock)
+                {
+                    if (PacketQueue.Count != 0)
+                    {
+                        shouldSleep = false;
+                    }
+                } 
+                if (shouldSleep)
+                {
+                    Thread.Sleep(250);
+                }
+                else // should process the queue
+                {
+
+                }
+            }
+        }
+        private void device_OnPacketArrival(object sender, CaptureEventArgs e)
+        {
+            // print out periodic statistics about this device
+            var Now = DateTime.Now;
+            byte[] data = e.Packet.Data;
+            StringBuilder ret = new StringBuilder();
+            foreach (byte b in data)
+            {
+                //{0:X2} 大写
+                ret.AppendFormat("{0:x2}", b);
+            }
+            var hex = ret.ToString(); 
+            txtRecv.AppendText(Now+":-"+ data .Length+ "bytes.\r\n" + ret.ToString() + "\r\n");
+            
+        }
+
+        private void CloseNet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenNet.Enabled = true;
+                CloseNet.Enabled = false;
+                device.StopCapture(); 
+                device.Close();
+            }catch(Exception exp)
+            {
+                device.Close();
+            }
         }
     }
 }
